4ace4e6bb1c3c4e7b7f6499e6b9ae6d4fd4a0119
Jacqueline S <104805706+jupiterskies@users.noreply.github.com>
2024-08-08 09:57:33 -0500

    Create dynamicArrays1.cpp

---
+++ 0. Dynamic Arrays/dynamicArrays1.cpp
@@ -0,0 +1,41 @@
+#include <iostream> 
+using namespace std; 
+
+class Card{
+
+string cardNumber, cardName; 
+double cardBalance; 
+
+public: 
+
+// card constructor
+card(string number, string name, double balance){
+  cardNumber = number; 
+  cardName = name; 
+  cardBalance = balance; 
+}
+
+// Card functions (public) 
+string getNumber(){ return cardNumber;}
+string getName(){ return cardName; }
+double getBalance(){ return cardBalance; } 
+void setBalance(double balance){ cardBalance = balance; }
+};
+
+int  main(){
+
+  // allocating memory for 3 "Card" type objects storing them in a pointer array named "wallet" 
+  card **wallet = new Card *[3]; 
+
+  wallet[0] = new Card("Jacob", "1234", 1000.0); 
+  wallet[1] = new Card("Elijah", "2345", 12000.01); 
+  wallet[2] = new Card("Ellie", "3434" , 1500.58); 
+
+  // lets print out these cards stored in "wallet" pointer array
+  for(int i = 0; i<3; i++){
+    cout<<"this is wallet <<i<<"and their name is"<<wallet[i]->cardName<<"."<<"There Balance is"<<wallet[i]->cardBalance<<"\n"; 
+  
+}
+
+
+}
