682d0ec905bf56939694fb8b233d0bb24a651c5f
Jacqueline S <104805706+jupiterskies@users.noreply.github.com>
2024-08-08 10:17:57 -0500

    formatting code

--- 0. Dynamic Arrays/dynamicArrays1.cpp
+++ 0. Dynamic Arrays/dynamicArrays1.cpp
@@ -1,41 +1,40 @@
-#include <iostream> 
-using namespace std; 
+#include <iostream>
+using namespace std;
 
-class Card{
+class Card
+{
 
-string cardNumber, cardName; 
-double cardBalance; 
+    string cardNumber, cardName;
+    double cardBalance;
 
-public: 
+public:
+    // card constructor
+    Card(string name, string number, double balance){
+        cardNumber = number;
+        cardName = name;
+        cardBalance = balance;
+    }
 
-// card constructor
-Card(string name, string number, double balance){
-  cardNumber = number; 
-  cardName = name; 
-  cardBalance = balance; 
-}
-
-// Card functions (public) 
-string getNumber(){ return cardNumber;}
-string getName(){ return cardName; }
-double getBalance(){ return cardBalance; } 
-void setBalance(double balance){ cardBalance = balance; }
+    // Card functions (public)
+    string getNumber() { return cardNumber; }
+    string getName() { return cardName; }
+    double getBalance() { return cardBalance; }
+    void setBalance(double balance) { cardBalance = balance; }
 };
 
-int  main(){
+int main()
+{
 
-  // allocating memory for 3 "Card" type objects storing them in a pointer array named "wallet" 
-  Card **wallet = new Card *[3]; 
+    // allocating memory for 3 "Card" type objects storing them in a pointer array named "wallet"
+    Card **wallet = new Card *[3];
 
-  wallet[0] = new Card("Jacob", "1234", 1000.0); 
-  wallet[1] = new Card("Elijah", "2345", 12000.01); 
-  wallet[2] = new Card("Ellie", "3434" , 1500.58); 
+    wallet[0] = new Card("Jacob", "1234", 1000.0);
+    wallet[1] = new Card("Elijah", "2345", 12000.01);
+    wallet[2] = new Card("Ellie", "3434", 1500.58);
 
-  // lets print out these cards stored in "wallet" pointer array
-  for(int i = 0; i<3; i++){
-    cout<<"This is wallet "<<i<<" and their name is "<<wallet[i]->getName()<<"."<<"Their Balance is "<<wallet[i]->getBalance()<<"\n"; 
-  
-}
-
-
+    // lets print out these cards stored in "wallet" pointer array
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "This is wallet " << i << " and their name is " << wallet[i]->getName() << "." << "Their Balance is " << wallet[i]->getBalance() << "\n";
+    }
 }
