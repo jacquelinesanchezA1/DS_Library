7716a2c4c4378d71bb486ea17098ae15f2032ace
Jacqueline S <104805706+jacquelinesanchezA1@users.noreply.github.com>
2024-10-25 02:04:02 -0500

    Update calculator_stack.cpp

--- 3.Stacks/stack_applications/calculator_stack.cpp
+++ 3.Stacks/stack_applications/calculator_stack.cpp
@@ -1,79 +1,84 @@
 #include <iostream>
 #include <stack>
+#include <string>
 using namespace std; 
 
 /* credit : https://leetcode.com/problems/basic-calculator/solutions/62432/ac-c-solution-with-two-stacks/ */
 
-class calculator{
-
-
-    public: 
-
-    int performOperation(int x, int y, char op){
-        if(op == '+'){
-            return x+y; 
-        }else{
-            return x-y; 
+class Calculator {
+public:
+    int performOperation(int x, int y, char op) {
+        switch (op) {
+            case '+': return x + y;
+            case '-': return x - y;
+            case '*': return x * y;
+            case '/': 
+                if (y == 0) throw runtime_error("division by zero");
+                return x / y;
+            default: return 0;
         }
     }
 
-
-
-    int calculate(string str){
-        stack<int> resultStack; 
-        stack<char> operatorStack; 
-
-        for(int i = 0; i<str.size(); i++){
-            char c = str[i]; 
-
-            // skip whitespaces
-            if(c == ' ') continue; 
-
-
-            // if handle unary '-' operator
-            if(c == '-' && (i-1<0 || str[i-1] == '(')){
-
-                resultStack.push(0); 
-            }
-
-            if(c == '*' || c== '/' || c == '+' || c == '-' || c == '('){
-
-            operatorStack.push(c); 
-            continue; }
-
-
-
-        // pop '('
-        if(c == ')') operatorStack.pop(); 
-
-        if(isdigit(c)){
-            int value = c-'0'; 
-            while(i + 1<str.size() && isdigit(str[i+1])){
-                value = (value*10) + (str[i+1] - '0'); 
-                i++; 
-            }
-            resultStack.push(value); 
-        }
-
-        if(!operatorStack.empty() && (operatorStack.top() == '+' || operatorStack.top() == '-' || operatorStack.top() == '*' || operatorStack.top() == '/')){
-            
-            int y = resultStack.top(); 
-            resultStack.pop(); 
-
-            int x = resultStack.top(); 
-            resultStack.pop(); 
-
-            char op = operatorStack.top(); 
-            operatorStack.pop(); 
-
-            int result = performOperation(x, y, op); 
-            resultStack.push(result); 
-        }
+    int precedence(char op) {
+        if (op == '+' || op == '-') return 1;
+        if (op == '*' || op == '/') return 2;
+        return 0;
     }
 
-    return resultStack.top(); 
- }
+    int calculate(string str) {
+        stack<int> resultStack;
+        stack<char> operatorStack;
 
+        for (int i = 0; i < str.size(); i++) {
+            char c = str[i];
+            if (c == ' ') continue;
+
+            if (c == '-' && (i - 1 < 0 || str[i - 1] == '(')) {
+                resultStack.push(0);
+            }
+
+            if (isdigit(c)) {
+                int value = c - '0';
+                while (i + 1 < str.size() && isdigit(str[i + 1])) {
+                    value = (value * 10) + (str[i + 1] - '0');
+                    i++;
+                }
+                resultStack.push(value);
+            } 
+            else if (c == '(') {
+                operatorStack.push(c);
+            } 
+            else if (c == ')') {
+                while (!operatorStack.empty() && operatorStack.top() != '(') {
+                    evaluateTop(resultStack, operatorStack);
+                }
+                operatorStack.pop();  // remove '('
+            } 
+            else if (c == '+' || c == '-' || c == '*' || c == '/') {
+                while (!operatorStack.empty() && precedence(operatorStack.top()) >= precedence(c)) {
+                    evaluateTop(resultStack, operatorStack);
+                }
+                operatorStack.push(c);
+            }
+        }
+
+        while (!operatorStack.empty()) {
+            evaluateTop(resultStack, operatorStack);
+        }
+
+        return resultStack.top();
+    }
+
+private:
+    void evaluateTop(stack<int>& resultStack, stack<char>& operatorStack) {
+        int y = resultStack.top();
+        resultStack.pop();
+        int x = resultStack.top();
+        resultStack.pop();
+        char op = operatorStack.top();
+        operatorStack.pop();
+        resultStack.push(performOperation(x, y, op));
+    }
 };
 
 
@@ -89,13 +94,16 @@ int main(){
     // 23+3-1*5
     // 23+3-5 = 26-5 = 21
 
-    int result1 = stackA.calculate("23+3-1*(2+3)"); 
+    int result1 = stackA.calculate("23+3-1*(2+3)");  // expected: 23+3-5 = 21
     cout<< result1;
 
     cout<<endl; 
 
     // correct
-    int result2 = stackA.calculate("(5+7)-9"); 
+    int result2 = stackA.calculate("(5+7)-9");  // expected: 3
     cout<< result2;  
+
+    int result3 = calc.calculate("23+3*4");
+    cout << "result 3: " << result3 << endl; // expected: 23 + (3*4) = 35
 
 }
